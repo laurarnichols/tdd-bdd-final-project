aa8452425d6aa1eb8808137201fceb809d6d3bfd
Laura Nichols <laurarnichols5@gmail.com>
2024-12-16 20:08:33 -0500

    Remove old comment

--- service/routes.py
+++ service/routes.py
@@ -86,9 +86,6 @@ def create_products():
 
     message = product.serialize()
 
-    #
-    # Uncomment this line of code once you implement READ A PRODUCT
-    #
     location_url = url_for("get_products", product_id=product.id, _external=True)
     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
